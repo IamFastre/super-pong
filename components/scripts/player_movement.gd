46404c2a01018032fe44f3f0d6aa9c2bb5ef205c
IamFastre <61952761+IamFastre@users.noreply.github.com>
2024-09-07 21:23:39 +0300

    slightly less expensive player movement

--- components/scripts/player_movement.gd
+++ components/scripts/player_movement.gd
@@ -2,12 +2,25 @@ class_name PlayerMovement extends PaddleMovement
 
 @export var id:PlayerInfo.ID = PlayerInfo.ID.P1
 
-func _physics_process(delta:float) -> void:
-	if id == PlayerInfo.ID.P1:
-		direction = Input.get_axis("p1_up", "p1_down")
-		is_sprinting = Input.get_action_strength("p1_sprint")
-	elif id == PlayerInfo.ID.P2:
-		direction = Input.get_axis("p2_up", "p2_down")
-		is_sprinting = Input.get_action_strength("p2_sprint")
+var get_input:Callable
 
+func player1_input():
+	direction = Input.get_axis("p1_up", "p1_down")
+	is_sprinting = Input.get_action_strength("p1_sprint")
+
+func player2_input():
+	direction = Input.get_axis("p2_up", "p2_down")
+	is_sprinting = Input.get_action_strength("p2_sprint")
+
+#=====================================================================#
+
+func _ready() -> void:
+	if id == PlayerInfo.ID.P1:
+		get_input = player1_input
+	elif id == PlayerInfo.ID.P2:
+		get_input = player2_input
+
+func _physics_process(delta:float) -> void:
+	if get_input:
+		get_input.call()
 	move(delta)
