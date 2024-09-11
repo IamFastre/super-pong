bc875313ab66751c90af3b984b83ff8a585865ee
IamFastre <61952761+IamFastre@users.noreply.github.com>
2024-09-11 06:02:20 +0300

    ball speed get higher the more it's hit by paddles now

--- scenes/scripts/ball.gd
+++ scenes/scripts/ball.gd
@@ -1,12 +1,20 @@
 class_name BallNode extends CharacterBody2D
 
-@export var speed_multiplier:float = 750
 ## Initial direction of the ball, leave (0, 0) for random.
 @export var initial_direction:Vector2 = Vector2.ZERO
+@export_group("Speed", "speed_")
+@export var speed_min:float = 500
+@export var speed_max:float = 2000
+@export var speed_multiplier:float = 750 :
+	get: return speed_multiplier
+	set(value): speed_multiplier = clampf(value, speed_min, speed_max)
+@export var speed_hit_interval:int = 2
+@export var speed_step:float = 25
 
 @onready var wait_timer:Timer = $WaitTime
 
 var direction:Vector2 = Vector2.ZERO
+var hit_count:int = 0
 
 func random_direction() -> Vector2:
 	var x = [-1, 1].pick_random()
@@ -25,6 +33,10 @@ func handle_collision(collision:KinematicCollision2D) -> void:
 	direction = reflect(contact_direction)
 
 	if collider is PaddleNode:
+		hit_count += 1
+		if hit_count % speed_hit_interval == 0:
+			speed_multiplier += speed_step
+
 		var paddle := collider as PaddleNode
 		var rot_dir:float = 1 if direction.x == 0.0 else direction.x / abs(direction.x)
 		var strength:float = paddle.movement.velocity / paddle.movement.speed_multiplier / 2
@@ -46,7 +58,7 @@ func _on_wait_timeout() -> void:
 
 #=====================================================================#
 
-func _physics_process(delta:float) -> void:
+func _process(delta:float) -> void:
 	var collision := move_and_collide(direction * speed_multiplier * delta)
 
 	if collision:
