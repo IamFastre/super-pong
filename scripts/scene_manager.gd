5b274199e100d6016a738824f89b471d214271ff
IamFastre <61952761+IamFastre@users.noreply.github.com>
2024-09-12 08:26:13 +0300

    small code tweaks

--- scripts/scene_manager.gd
+++ scripts/scene_manager.gd
@@ -6,33 +6,31 @@ enum Scene {
 	# SuperPong,
 }
 
-const MAIN_MENU:PackedScene = preload("res://scenes/levels/main_menu.tscn")
+const MAIN_MENU:PackedScene    = preload("res://scenes/levels/main_menu.tscn")
 const CLASSIC_GAME:PackedScene = preload("res://scenes/levels/classic_game.tscn")
 
 var current:Node
 
 func switch(scene:Scene) -> void:
-	call_deferred("_undeferred_switch", scene)
+	call_deferred("_deferred_switch", scene)
 
 #=====================================================================#
 
-func _undeferred_switch(scene:Scene) -> void:
+func _deferred_switch(scene:Scene) -> void:
 	if current:
 		current.free()
 
-	var new_scene:Node
-	match scene:
-		Scene.MainMenu:
-			new_scene = MAIN_MENU.instantiate()
-		Scene.ClassicGame:
-			new_scene = CLASSIC_GAME.instantiate()
-		_:
-			assert(false, "Unknown scene id")
+	var s:PackedScene
 
-	current = new_scene
+	match scene:
+		Scene.MainMenu:    s = MAIN_MENU
+		Scene.ClassicGame: s = CLASSIC_GAME
+		_: assert(false, "Unknown scene id")
+
+	current = s.instantiate()
+
 	get_tree().root.add_child(current)
 	get_tree().current_scene = current
-	print(get_tree().current_scene)
 
 #=====================================================================#
 
