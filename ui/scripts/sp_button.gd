0c1dc9b51ee2e59594ef43e6b5b775f6719459eb
IamFastre <61952761+IamFastre@users.noreply.github.com>
2024-09-13 03:28:09 +0300

    ui stuff

--- ui/scripts/sp_button.gd
+++
@@ -1,9 +0,0 @@
-class_name SPButton extends Button
-
-func _on_button_down() -> void:
-	if text:
-		text = "> %s <" % text
-
-func _on_button_up() -> void:
-	if text and text.begins_with(">") and text.ends_with("<"):
-		text = text.substr(2, text.length() - 4)
