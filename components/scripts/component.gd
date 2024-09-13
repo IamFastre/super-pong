dfeaeeb8dda9971429aabf49207210f91e40d499
IamFastre <61952761+IamFastre@users.noreply.github.com>
2024-09-13 03:28:26 +0300

    new press effect component

--- components/scripts/component.gd
+++ components/scripts/component.gd
@@ -1,4 +1,4 @@
 class_name ComponentNode extends Node
 
-@export var custom_parent:Node2D
-@onready var parent = custom_parent if custom_parent else get_parent() as Node2D
+@export var custom_parent:Node
+@onready var parent = custom_parent if custom_parent else get_parent()
