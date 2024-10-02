5112ecf44abad6b52631bdbd96ff99eae3fba409
IamFastre <61952761+IamFastre@users.noreply.github.com>
2024-10-02 22:10:53 +0300

    util functions

--- scripts/utilities.gd
+++ scripts/utilities.gd
@@ -2,7 +2,40 @@ extends Node
 
 @onready var screensize:Vector2 = get_viewport().content_scale_size
 @onready var center:Vector2 = screensize / 2
+@onready var system:System = get_system()
 @onready var has_touch_screen:bool = DisplayServer.is_touchscreen_available()
+
+#=====================================================================#
+
+enum System {
+	GOD_KNOWS,
+	WINDOWS,
+	POSIX,
+	OSX,
+	IOS,
+	ANDROID,
+	WEB,
+}
+
+#=====================================================================#
+
+func get_system() -> System:
+	match OS.get_name():
+		"Windows":
+			return System.WINDOWS
+		"Linux", "FreeBSD", "NetBSD", "OpenBSD", "BSD":
+			return System.POSIX
+		"macOS":
+			return System.OSX
+		"iOS":
+			return System.IOS
+		"Android":
+			return System.ANDROID
+		"Web":
+			return System.WEB
+		_: # Unknown
+			OS.crash("I'm racist against this specific OS")
+			return System.GOD_KNOWS
 
 #=====================================================================#
 
