b5903b560f274cae56023d4d9f63c1de1334035e
IamFastre <61952761+IamFastre@users.noreply.github.com>
2024-10-05 20:22:11 +0300

    made movement clamp take account for height
    I swear it was a feature that it doesn't but my sister said it was shit so

--- components/movement_clamp.gd
+++ components/movement_clamp.gd
@@ -1,7 +1,14 @@
 class_name MovementClamp extends Component
 
+@export var shape:CollisionShape2D
+
+var height:float :
+	get: return shape.shape.get_rect().size.y
+
+#=====================================================================#
+
 func _ready():
 	process_priority = 2
 
 func _process(_delta:float) -> void:
-	parent.position.y = clampf(parent.position.y, 0, Utilities.screensize.y)
+	parent.position.y = clampf(parent.position.y, height / 2, Utilities.screensize.y - height / 2)
